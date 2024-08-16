ec483cd268dc77617f4d46f1807baeadd55f85bc
Janko Jovičić <jankojovicicwork@gmail.com>
2024-08-16 12:42:25 +0200

    Removing problematic requirment.

--- freppledb/erpconnectiontest/utils.py
+++ freppledb/erpconnectiontest/utils.py
@@ -25,20 +25,4 @@ from django.db import DEFAULT_DB_ALIAS
 
 
 def getERPconnection(database=DEFAULT_DB_ALIAS):
-    """
-    Customize this method to connect to the ERP database.
-
-    The example here uses Microsoft ADO to connect from a windows machine to a database.
-    The site https://www.connectionstrings.com/ has a handy summary of the syntax of the
-    connection strings.
-
-    To improve configurability of the connector we can use either:
-      a) Parameters stored in the frePPLe parameter table.
-         Benefit is that end user can then easily change these.
-      b) Settings in the frePPLe djangosettings file.
-         For storing password and other security sensitive information this file is better.
-    """
-    import adodbapi
-
-    connectionstring = "Provider=SQLNCLI11;Server=localhost;Database=acutec;User Id=acutec;Password=acutec;"
-    return adodbapi.connect(connectionstring, timeout=600)
+    return
